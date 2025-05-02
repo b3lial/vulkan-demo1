70a40277987b94f8ecf6b55e40307f1e2c60d667
Dr. Christian Ammann <christian.ammann@phobosys.de>
2025-05-02 11:51:06 +0200

    refactoring

---
+++ src/vulkandemoapplication.hpp
@@ -0,0 +1,43 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <cstdlib>
+#include <vector>
+#include <string>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class VulkanDemoApplication {
+ public:
+    void run();
+
+ private:
+    void initWindow();
+    VkShaderModule createShaderModule(const std::vector<char>& code);
+    std::vector<char> readFile(const std::string& filename);
+    void createGraphicsPipeline();
+    void initVulkan();
+    void mainLoop();
+    void cleanup();
+
+    GLFWwindow* window;
+    VkInstance instance;
+    VkSurfaceKHR surface;
+    VkPhysicalDevice physicalDevice;
+    VkDevice device;
+    VkQueue graphicsQueue;
+    uint32_t queueFamilyIndex;
+    VkSwapchainKHR swapchain;
+    std::vector<VkImage> swapchainImages;
+    std::vector<VkImageView> swapchainImageViews;
+    VkFormat swapchainImageFormat;
+    VkRenderPass renderPass;
+    VkPipelineLayout pipelineLayout;
+    VkPipeline graphicsPipeline;
+    std::vector<VkFramebuffer> swapchainFramebuffers;
+    VkCommandPool commandPool;
+    std::vector<VkCommandBuffer> commandBuffers;
+    VkSemaphore imageAvailableSemaphore;
+    VkSemaphore renderFinishedSemaphore;
+};
